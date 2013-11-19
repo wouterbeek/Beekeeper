a5c4e2b4ea3d3f53bdfd2f10890018976e5dbd27
Wouter Beek <me@wouterbeek.com>
2013-11-19 23:20:11 +0100

    Rewrote startup (decoupled from dotcloud).

--- load.pl
+++ load.pl
@@ -1,28 +1,16 @@
-% Non-debug load file for the Beekeeper project.
+% The load file for the Beekeeper project.
 
-project_name('Beekeeper').
+:- multifile(user:project_name/1).
+user:project_name('Beekeeper').
 
 :- initialization(load_bk).
 
 load_bk:-
+  % Entry point.
   source_file(load_bk, ThisFile),
-  file_directory_name(ThisFile, ThisDirectory),
-  assert(user:file_search_path(project, ThisDirectory)),
-  assert(user:file_search_path(bk, ThisDirectory)),
-  assert(user:file_search_path(data, bk('Data'))),
+  file_directory_name(ThisFile, ThisDir),
   
-  % Load the PGC.
-  assert(user:file_search_path(pgc, bk('PGC'))),
-  (
-    predicate_property(debug_project, visible)
-  ->
-    ensure_loaded(pgc(debug))
-  ;
-    ensure_loaded(pgc(load))
-  ),
-  
-  assert(user:file_search_path(lib, bk(lib))),
-  
-  use_module(bk(bk_web)),
-  use_module(bk(how)).
+  % Beekeeper
+  assert(user:file_search_path(bk, ThisDir)),
+  use_module(bk(bk_web)).
 
